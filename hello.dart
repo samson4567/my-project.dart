619750aa924ec74c21cd66da753aa7f8d4b6ec16
samson4567 <102233394+samson4567@users.noreply.github.com>
2022-06-27 19:55:42 +0100

    Add files via upload

---
+++ hello.dart
@@ -0,0 +1,6 @@
+void main() {
+  print(
+      '''livinf world class like never before!imagine u are the best of all kind 
+  pulling up the bags just becuse of money! we ourght to have confirmed life
+  life can be so hard! just keep going''');
+}
---
+++ simple_addition.dart
@@ -0,0 +1,14 @@
+/// this is a simple task to print the result of an addition operation
+/// the result should read `numberOne + numberTwo = result`
+/// an example is if the user enters 2 and 3 the result should be `2 + 3 = 5 `
+///
+/// to run code paste `dart basic_arithmetic_operations/simple_addition.dart ` in the terminal
+void main() {
+  int numberOne = 24;
+  int numberTwo = 33;
+
+  int result = numberOne + numberTwo;
+
+  print('${numberOne} + $numberTwo = $result');
+ 
+}
