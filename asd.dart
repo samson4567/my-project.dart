dcfb2ec291e3f69d3591c24b05406c907eec7628
samson4567 <102233394+samson4567@users.noreply.github.com>
2022-06-27 19:29:55 +0100

    Add files via upload

---
+++ asd.dart
@@ -0,0 +1,10 @@
+import 'dart:math';
+
+void main() {
+  double price = 5500.5;
+
+  String samson = 'dfgshgjsfhghsrwury ${max(12, 3)}';
+  String name = 'samson';
+
+  print("djvskjgsdfkjgsdfkjjdjf dfdjdfjkdfjd ${name.length}");
+}
